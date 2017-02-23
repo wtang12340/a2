f340fce09e1e3d57420727d8f64dedd71d0df1b1
William Tang <wtang12340@gmail.com>
2017-02-23 14:53:53 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password Generator</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/william.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Password Generator</h1>
+
+	</div>
+</body>
+</html>
